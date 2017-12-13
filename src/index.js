afd6534e04440b971f545412b9031752f668be7a
pg <pawelgabryelewicz@users.noreply.github.com>
2017-12-13 11:00:42 +0100

    Initial repository version, 2.0.0

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// G.sel 2.0.0.20171106
+// Selector cache
+// Copyright (C) 2013-2017 ConsidoNet Solutions / www.considonet.com
+// Released under MIT Licence
+
+/*
+VERSION HISTORY
+2.0.0.20171106
++ Modular ES6 rewrite
+
+1.0.3.20170529 @pg
++ ES6
+
+1.0.1.20170510 @pg
++ Code refactor, namespace for jQuery
+
+1.0.0.20170126 @pg
++ Initial version
+
+*/
+
+import $ from 'jquery';
+
+export const $window = $(window);
+export const $html = $("html");
+export const $root = $("html,body");
+export const $body = $(document.body);
+export const $document = $(document);
