e18a66d1d7a0d6e5633f5690c7aba056e60326c1
pg <pawelgabryelewicz@users.noreply.github.com>
2018-11-29 17:40:00 +0100

    Recompilation

--- dist/index.js
+++ dist/index.js
@@ -1,46 +1,21 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.$document = exports.$body = exports.$root = exports.$html = exports.$window = undefined;
+Object.defineProperty(exports, '__esModule', { value: true });
 
-var _jquery = require("jquery");
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var _jquery2 = _interopRequireDefault(_jquery);
+var $ = _interopDefault(require('jquery'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+// G.sel 3.1.0
 
-var $window = exports.$window = (0, _jquery2.default)(window); // G.sel 3.0.0
-// Selector cache
-// Copyright (C) 2013-2018 ConsidoNet Solutions / www.considonet.com
-// Released under MIT Licence
+var $window = $(window);
+var $html = $("html");
+var $root = $("html,body");
+var $body = $(document.body);
+var $document = $(document);
 
-/*
-VERSION HISTORY
-3.0.0 (20180718) @pg
-+ Switched to semver
-* Dist package now transpiled from ES6 (compatibility with building environments not transpiling node_modules)
-+ Source linted with tslint
-
-2.0.1.20180214 @pg
-+ TypeScript declarations
-
-2.0.0.20171106
-+ Modular ES6 rewrite
-
-1.0.3.20170529 @pg
-+ ES6
-
-1.0.1.20170510 @pg
-+ Code refactor, namespace for jQuery
-
-1.0.0.20170126 @pg
-+ Initial version
-
-*/
-
-var $html = exports.$html = (0, _jquery2.default)("html");
-var $root = exports.$root = (0, _jquery2.default)("html,body");
-var $body = exports.$body = (0, _jquery2.default)(document.body);
-var $document = exports.$document = (0, _jquery2.default)(document);
+exports.$window = $window;
+exports.$html = $html;
+exports.$root = $root;
+exports.$body = $body;
+exports.$document = $document;
